be516391ca04d466cb0a2423c63f6ef9bed9bc72
PICOUET Vincent <vincent.picouet@lam.fr>
2018-08-15 17:54:43 -0600

    change requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 requires = ['numpy >=1.8', 
             'scipy >=0.14', 
@@ -11,9 +12,9 @@ requires = ['numpy >=1.8',
 setup(
     name='DS9functions',
     version='0.1dev',
-    packages=['DS9functions',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     install_requires = requires,
+    packages = find_packages(),
     author_email = 'vincent.picouet@lam.fr',
     description  = 'LAM AIT/quicklook functions proposal for FIREBall-2',
 )
