b2595b2c81756e4a61434cdec929c65fa14ffea7
PICOUET Vincent <vincent.picouet@lam.fr>
2019-09-17 15:54:42 +0200

    New version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ entry_points['console_scripts'] = ['DS9Utils = DS9FireBall.DS9Utils:main']
 data = { "DS9FireBall": ["FireBall.ds9.ans","dygraph-combined_new.js", "Slits/*","Targets/*","Mappings/*","Regions/*","CSVs/*","config/*","Sextractor/*"]}
 
 
-version = '2.74dev'
+version = '2.75dev'
 
 if sys.version_info.major == 3:
     setup(
